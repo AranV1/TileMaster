4e1ed4b80b0295d1206090f01377f8853a993170
AranV1 <dominik.szczurek@microsoft.wsei.edu.pl>
2025-10-14 21:43:39 +0200

    JumpV2 and Ladder

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,20 +5,27 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float moveSpeed = 5f;
     [SerializeField] float jumpSpeed = 5f;
+    [SerializeField] float climbSpeed = 5f;
+
 
     Vector2 moveInput;
     Rigidbody2D myRigidbody;
     Animator anim;
+    CapsuleCollider2D capsuleCl;
+    float gravityScaleAtStart;
 
     void Start()
     {
+        capsuleCl = GetComponent<CapsuleCollider2D>();
         myRigidbody = GetComponent<Rigidbody2D>();
         anim = GetComponent<Animator>();
+        gravityScaleAtStart = myRigidbody.gravityScale;
     }
     void Update()
     {
         Run();
         FlipSprite();
+        ClimbLadder();
     }
 
     void OnMove(InputValue value)
@@ -28,7 +35,8 @@ public class PlayerMovement : MonoBehaviour
 
     void OnJump(InputValue value)
     {
-        if(value.isPressed)
+        if (!capsuleCl.IsTouchingLayers(LayerMask.GetMask("Ground"))) { return; } //sprawdza czy gracz stoi na ziemi
+        if (value.isPressed)
         {
             myRigidbody.linearVelocity += new Vector2(0f, jumpSpeed);
         }
@@ -36,8 +44,8 @@ public class PlayerMovement : MonoBehaviour
 
     void Run()
     {
-        myRigidbody.linearVelocity = new Vector2(moveInput.x*moveSpeed, myRigidbody.linearVelocity.y); //myRigidbody.linearVelocity.y oznacza ze w osi y obiekt dziala
-                                                                                                       //zgodnie z fizyka (np grawitacja)
+        myRigidbody.linearVelocity = new Vector2(moveInput.x * moveSpeed, myRigidbody.linearVelocity.y); //myRigidbody.linearVelocity.y oznacza ze w osi y obiekt dziala
+                                                                                                         //zgodnie z fizyka (np grawitacja)
         anim.SetBool("isRunning", Mathf.Abs(myRigidbody.linearVelocity.x) > Mathf.Epsilon);
 
     }
@@ -49,5 +57,16 @@ public class PlayerMovement : MonoBehaviour
         {
             transform.localScale = new Vector2(Mathf.Sign(myRigidbody.linearVelocity.x), 1f);
         }
+    }
+
+    void ClimbLadder()
+    {
+        if (!capsuleCl.IsTouchingLayers(LayerMask.GetMask("Ladder")))
+        {
+            myRigidbody.gravityScale = gravityScaleAtStart;
+            return;
+        }
+        myRigidbody.gravityScale = 0f;
+        myRigidbody.linearVelocity = new Vector2(myRigidbody.linearVelocity.x, moveInput.y * climbSpeed);
     }
 }
