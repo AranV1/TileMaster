89f87a8c505df869a1d1d924f316414daa4a85f5
AranV1 <dominik.szczurek@microsoft.wsei.edu.pl>
2025-10-20 19:46:39 +0200

    Levels Loading and First try of Storing data through game

---
+++ Assets/Scripts/GameSession.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class GameSession : MonoBehaviour
+{
+    private void Awake()
+    {
+        int numberGameSessions = FindObjectsByType<GameSession>(FindObjectsSortMode.None).Length;
+        if (numberGameSessions > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
